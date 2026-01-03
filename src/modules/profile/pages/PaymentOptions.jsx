c729378886a4cb6345107c20848c0d988891d2d9
vatsamishraa <vatsalmishra88@gmail.com>
2026-01-03 17:23:45 +0530

    desktop mode improved

--- src/modules/profile/pages/PaymentOptions.jsx
+++ src/modules/profile/pages/PaymentOptions.jsx
@@ -105,6 +105,9 @@ const PaymentOptions = () => {
       <MobileCommonHeaderthree title="Payment Options" />
       <section className="zero-padding-section">
         <div className={styles.panelWrapper}>
+          <div className={styles.right}>
+            <ProfileCard />
+          </div>
           <div className={styles.left}>
             <div className={styles.header}>
               <button className={styles.addBtn} onClick={handleAddNew}>
@@ -132,7 +135,7 @@ const PaymentOptions = () => {
               </button>
             </div>
 
-            <div className={styles.paymentsList}>
+            <div className={`${styles.paymentsList} profile-content-scroll`}>
               {/* UPI Tab Content */}
               {selectedMethod === 0 && (
                 <>
@@ -226,10 +229,6 @@ const PaymentOptions = () => {
                 </>
               )}
             </div>
-          </div>
-
-          <div className={styles.right}>
-            <ProfileCard />
           </div>
         </div>
       </section>
